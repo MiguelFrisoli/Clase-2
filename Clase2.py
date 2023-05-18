ca54467f68ae6766ad604f0c3ad38034968ab130
MiguelFrisoli <frisolimiguel@hotmail.com>
2023-05-18 17:04:26 -0300

    comentario inicial

---
+++ Clase2.py
@@ -0,0 +1,9 @@
+"""Write a function that takes the base and height of a triangle and return its area"""
+
+def tri_area(base, height):
+    area = (float (base * height))/2
+    return print ("El Área del triangulo es igual a: ", area, "cm cuadrados")
+
+tri_area(7,9)
+
+
